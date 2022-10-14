b53537cda349e623aec01f73c2c5daf814315ee2
sruthy.fc <sruthy@faircodetech.com>
2022-10-14 14:44:43 +0530

    sports type images

--- lib/services/navigation/router.gr.dart
+++ lib/services/navigation/router.gr.dart
@@ -10,47 +10,32 @@
 //
 // ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i3;
-import 'package:flutter/material.dart' as _i4;
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:flutter/material.dart' as _i3;
 
-import '../../ui/login_screen/login_screen.dart' as _i2;
-import '../../ui/splash_screen/splash_screen.dart' as _i1;
+import '../../ui/student_basic_profile/sports_type_screen.dart' as _i1;
 
-class AppRouter extends _i3.RootStackRouter {
-  AppRouter([_i4.GlobalKey<_i4.NavigatorState>? navigatorKey])
+class AppRouter extends _i2.RootStackRouter {
+  AppRouter([_i3.GlobalKey<_i3.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i3.PageFactory> pagesMap = {
-    SplashScreen.name: (routeData) {
-      return _i3.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i1.SplashScreen());
-    },
-    LoginScreen.name: (routeData) {
-      return _i3.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i2.LoginScreen());
+  final Map<String, _i2.PageFactory> pagesMap = {
+    SportsTypeScreen.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i1.SportsTypeScreen());
     }
   };
 
   @override
-  List<_i3.RouteConfig> get routes => [
-        _i3.RouteConfig(SplashScreen.name, path: '/'),
-        _i3.RouteConfig(LoginScreen.name, path: '/')
-      ];
+  List<_i2.RouteConfig> get routes =>
+      [_i2.RouteConfig(SportsTypeScreen.name, path: '/')];
 }
 
 /// generated route for
-/// [_i1.SplashScreen]
-class SplashScreen extends _i3.PageRouteInfo<void> {
-  const SplashScreen() : super(SplashScreen.name, path: '/');
+/// [_i1.SportsTypeScreen]
+class SportsTypeScreen extends _i2.PageRouteInfo<void> {
+  const SportsTypeScreen() : super(SportsTypeScreen.name, path: '/');
 
-  static const String name = 'SplashScreen';
-}
-
-/// generated route for
-/// [_i2.LoginScreen]
-class LoginScreen extends _i3.PageRouteInfo<void> {
-  const LoginScreen() : super(LoginScreen.name, path: '/');
-
-  static const String name = 'LoginScreen';
+  static const String name = 'SportsTypeScreen';
 }
