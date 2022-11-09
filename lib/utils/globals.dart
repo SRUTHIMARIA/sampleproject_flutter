d4a24fa1e306721999785d951b7a3b84c5554f3c
sruthy.fc <sruthy@faircodetech.com>
2022-11-09 11:23:38 +0530

    globals

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_template/services/navigation/router.dart';
 class Globals {
 
   static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
   static BuildContext context() => navigatorKey.currentState!.context;
-// static final appRouter = AppRouter();
 }
