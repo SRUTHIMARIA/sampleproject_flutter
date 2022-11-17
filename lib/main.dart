5bb887c5dc7d35fef248350cab21bc635f0d9570
sruthy.fc <sruthy@faircodetech.com>
2022-11-17 10:12:51 +0530

    package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_template/providers/authentication_provider.dart';
-import 'package:flutter_template/ui/login_screen/login_screen.dart';
-import 'package:flutter_template/ui/scheduling_self_analysis/self_analyisis_notes.dart';
-import 'package:flutter_template/providers/authentication_provider.dart';
+
 import 'package:flutter_template/providers/providers.dart';
 import 'package:flutter_template/ui/splash_screen/splash_screen.dart';
 
