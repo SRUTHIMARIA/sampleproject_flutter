88078cef7126fd4c01a55d1cf1dc49b45d324413
sruthy.fc <sruthy@faircodetech.com>
2022-11-30 12:29:18 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_template/firebase_options.dart';
 import 'package:flutter_template/providers/providers.dart';
-import 'package:flutter_template/ui/homepage/Chat/chat_session.dart';
-import 'package:flutter_template/ui/student_basic_profile/sports_type_screen.dart';
 import 'package:flutter_template/utils/globals.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_template/providers/authentication_provider.dart';
-
-import 'package:flutter_template/providers/providers.dart';
 import 'package:flutter_template/ui/splash_screen/splash_screen.dart';
-
-import 'package:flutter_template/utils/globals.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
-import 'package:provider/provider.dart';
-import 'package:flutter_template/ui/feedback/feedback.dart';
-import 'package:flutter_template/ui/homepage/Chat/chat_screen.dart';
-import 'package:flutter_template/ui/subscriptions/subscriptions.dart';
-
-
-
 import 'package:responsive_framework/responsive_framework.dart';
 
 // Future main() async{
@@ -29,17 +15,17 @@ import 'package:responsive_framework/responsive_framework.dart';
 //   runApp(MyApp());
 // }
 
-
-void main()  async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MultiProvider(
-    providers: providers,
-    child: MyApp(),
-  ),
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    MultiProvider(
+      providers: providers,
+      child: MyApp(),
+    ),
   );
 
- Firebase.initializeApp();
+  Firebase.initializeApp();
   await Hive.initFlutter();
   // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await AuthenticationProvider.retrieveAuthUser();
@@ -59,17 +45,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       navigatorKey: Globals.navigatorKey,
       debugShowCheckedModeBanner: false,
-      builder: (context, child) => ResponsiveWrapper.builder(child,
-          maxWidth: 1200,
-          minWidth: 480,
-          defaultScale: true,
-          breakpoints: [
-            const ResponsiveBreakpoint.resize(480, name: MOBILE),
-            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-            const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
-            const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-          ],
-          background: Container(color: const Color(0xFFF5F5F5)),),
+      builder: (context, child) => ResponsiveWrapper.builder(
+        child,
+        maxWidth: 1200,
+        minWidth: 480,
+        defaultScale: true,
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(480, name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+          const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+        ],
+        background: Container(color: const Color(0xFFF5F5F5)),
+      ),
       title: 'Athlete Assist',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -78,7 +66,6 @@ class MyApp extends StatelessWidget {
       // home: ChangeNotifierProvider(
       //   create: (context) => DrawerScreenProvider(),
       //   child:  HomePage(),
-
     );
   }
 }
